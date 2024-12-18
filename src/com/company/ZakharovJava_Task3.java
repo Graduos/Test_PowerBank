85e7eff053f72533b1e7dcccf9fd7531fd6499b3
Graduos <lfavty.1999@gmail.com>
2024-12-18 22:04:19 +0300

    Aston

--- src/com/company/ZakharovJava_Task3.java
+++ src/com/company/ZakharovJava_Task3.java
@@ -2,10 +2,8 @@ package com.company;
 
     public class ZakharovJava_Task3 {
         public static void main(String[] args) {
-            // Заданный массив целых чисел
             int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-            // Вывод четных чисел
             System.out.println("Чётные числа в массиве:");
             for (int number : numbers) {
                 if (number % 2 == 0) {
